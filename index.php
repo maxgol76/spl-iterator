fa295eb596ef2597674fd25ae3fcf807127b9ade
maxgol76 <maxgol76@gmail.com>
2023-05-12 14:58:18 +0300

    step 2

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
-$iterator = new FilesystemIterator('main folder');
+$directory = (string)readline("Your directory: ");
+
+$iterator = new FilesystemIterator($directory);
 
 foreach($iterator as $entry) {
 
     print($entry->getFilename() . "\n");
 
      if ($entry->getType() === 'dir') {
-         $it = new FilesystemIterator('main folder/' . $entry->getFilename());
+         $it = new FilesystemIterator($directory . '/' . $entry->getFilename());
          foreach($it as $en) {
              print( ' - ' . $en->getFilename() . "\n");
          }
